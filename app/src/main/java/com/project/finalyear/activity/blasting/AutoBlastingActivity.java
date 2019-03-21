3bcadab6da8b4ebc28c097c2057cb5fe00971f94
shrirambaabu <shrirambaabu0902@gmail.com>
2019-03-21 16:29:26 +0530

    Splash Screen Added

--- app/src/main/java/com/project/finalyear/activity/blasting/AutoBlastingActivity.java
+++ app/src/main/java/com/project/finalyear/activity/blasting/AutoBlastingActivity.java
@@ -1,36 +1,31 @@
-package com.project.finalyear.activity;
+package com.project.finalyear.activity.blasting;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 
 import com.project.finalyear.R;
-import com.project.finalyear.activity.blasting.AutoBlastingActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 import static com.project.finalyear.utils.Utils.backButtonOnToolbar;
 
-public class BlastingHomeActivity extends AppCompatActivity {
+public class AutoBlastingActivity extends AppCompatActivity {
+
 
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
 
-    @BindView(R.id.auto_blasting_card)
-    CardView autoBlastingCardView;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_blasting_home);
+        setContentView(R.layout.activity_auto_blasting);
+
+
         ButterKnife.bind(this);
 
 
@@ -65,15 +60,5 @@ public class BlastingHomeActivity extends AppCompatActivity {
 
         finish();
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-    }
-
-
-    @OnClick(R.id.auto_blasting_card)
-    public void autoBlasting(){
-
-        Intent intent=new Intent(getApplicationContext(), AutoBlastingActivity.class);
-        startActivity(intent);
-        overridePendingTransition(R.anim.enter_right_to_left, R.anim.exit_left_to_right);
-
     }
 }
