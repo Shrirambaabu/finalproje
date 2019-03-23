b94e151668c29c6e2792233aef65ad2d1bbd4e9c
Shrirambaabu <shrirambaabu0902@gmail.com>
2019-03-23 15:04:51 +0530

    first build

--- app/src/main/java/com/project/finalyear/activity/blasting/abbrasive/hazards/DemobilizeActivity.java
+++ app/src/main/java/com/project/finalyear/activity/blasting/abbrasive/hazards/DemobilizeActivity.java
@@ -11,5 +11,6 @@ public class DemobilizeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demobilize);
+
     }
 }
