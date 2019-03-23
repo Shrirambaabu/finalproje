54ac55800fc6cb7887603c962f0526d5f8d1e755
Shrirambaabu <shrirambaabu0902@gmail.com>
2019-03-23 10:48:15 +0530

    first build

--- app/src/main/java/com/project/finalyear/activity/InfoActivity.java
+++ app/src/main/java/com/project/finalyear/activity/InfoActivity.java
@@ -1,45 +1,27 @@
 package com.project.finalyear.activity;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 
 import com.project.finalyear.R;
-import com.project.finalyear.activity.blasting.AutoBlastingActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 import static com.project.finalyear.utils.Utils.backButtonOnToolbar;
 
-public class BlastingHomeActivity extends AppCompatActivity {
-
+public class InfoActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
-
-    @BindView(R.id.auto_blasting_card)
-    CardView autoBlastingCardView;
-
-    @BindView(R.id.rock_blasting_card)
-    CardView rockBlastingCardView;
-
-    @BindView(R.id.abbrasive_blasting_card)
-    CardView abbrasiveBlastingCardView;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_blasting_home);
+        setContentView(R.layout.activity_info);
         ButterKnife.bind(this);
-
-
         backButtonOnToolbar(this);
         setupToolbar();
 
@@ -56,8 +38,6 @@ public class BlastingHomeActivity extends AppCompatActivity {
         }
     }
 
-
-
     @Override
     public boolean onSupportNavigateUp() {
         finish();
@@ -71,15 +51,5 @@ public class BlastingHomeActivity extends AppCompatActivity {
 
         finish();
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-    }
-
-
-    @OnClick(R.id.auto_blasting_card)
-    public void autoBlasting(){
-
-        Intent intent=new Intent(getApplicationContext(), AutoBlastingActivity.class);
-        startActivity(intent);
-        overridePendingTransition(R.anim.enter_right_to_left, R.anim.exit_left_to_right);
-
     }
 }
--- app/src/main/java/com/project/finalyear/activity/MainActivity.java
+++ app/src/main/java/com/project/finalyear/activity/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.project.finalyear.R;
@@ -22,9 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
-
-    @BindView(R.id.drawer_layout)
-    DrawerLayout mDrawerLayout;
 
     @BindView(R.id.card_one)
     CardView cardOne;
@@ -39,27 +38,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.about_ssra, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
     private void setupToolBar() {
 
-        toolbar.setTitle("Project");
+        toolbar.setTitle("SSRA");
         setSupportActionBar(toolbar);
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
-        }
     }
 
-
-    @OnClick(R.id.close)
-    public void closeDrawer() {
-        mDrawerLayout.closeDrawer(GravityCompat.START);
-    }
 
     @OnClick(R.id.card_one)
     public void cardOne() {
-        Intent intent=new Intent(getApplicationContext(),BlastingHomeActivity.class);
+        Intent intent = new Intent(getApplicationContext(), BlastingHomeActivity.class);
         startActivity(intent);
         overridePendingTransition(R.anim.enter_right_to_left, R.anim.exit_left_to_right);
 
@@ -67,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.card_two)
     public void cardTwo() {
-        Intent intent=new Intent(getApplicationContext(),SampleDetailsActivity.class);
+        Intent intent = new Intent(getApplicationContext(), SampleDetailsActivity.class);
         startActivity(intent);
         overridePendingTransition(R.anim.enter_right_to_left, R.anim.exit_left_to_right);
 
@@ -75,31 +72,24 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.ic_app_logo).setTitle("Exit");
+        builder.setMessage("Are you sure you want to Exit?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finishAffinity();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
 
-        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
-            mDrawerLayout.closeDrawer(GravityCompat.START);
-        } else  {
+        });
+        builder.setNegativeButton("No", null);
+        //builder.show();
+        AlertDialog dialog = builder.create();
+        dialog.show(); //Only after .show() was called
+        dialog.getButton(dialog.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.colorDeepBlue));
+        dialog.getButton(dialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.colorDeepBlue));
 
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setIcon(R.drawable.ic_app_logo).setTitle("Exit");
-            builder.setMessage("Are you sure you want to Exit?");
-            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    finishAffinity();
-                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                }
-
-            });
-            builder.setNegativeButton("No", null);
-            //builder.show();
-            AlertDialog dialog = builder.create();
-            dialog.show(); //Only after .show() was called
-            dialog.getButton(dialog.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.colorDeepBlue));
-            dialog.getButton(dialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.colorDeepBlue));
-
-
-        }
 
     }
 
@@ -109,8 +99,13 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
 
             case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
+            case R.id.info:
+                Intent intentSave = new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(intentSave);
+                overridePendingTransition(R.anim.enter_right_to_left, R.anim.exit_left_to_right);
+                return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
